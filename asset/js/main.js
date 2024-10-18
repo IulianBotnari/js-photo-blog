b13d01f2883c3904e0af2ed39c31b3bdd60e15a1
IulianBotnari <iulianbotnari93@gmail.com>
2024-10-18 11:32:54 +0200

    add some js

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,8 +1,27 @@
+
+
+const mainEl = document.querySelector("main")
 axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
 .then(response => {
-    console.log(response);
+    let items
+    items = response.data
+
+    items.forEach(element => {
+        let itemsEl = element
+
+        let {albumId, id, title, url, thumbnailUrl} = itemsEl
+
+        console.log(itemsEl)
+
+        console.log(albumId);
+        
+
+        
+    })
     
     
 })
+
+
 
 
