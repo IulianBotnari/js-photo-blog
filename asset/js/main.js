5b20bbecf3fcb562cf554a82a33be26b454e8d1a
IulianBotnari <iulianbotnari93@gmail.com>
2024-10-18 11:42:55 +0200

    Commit per fabio

--- asset/js/main.js
+++ asset/js/main.js
@@ -2,25 +2,35 @@
 
 const mainEl = document.querySelector("main")
 axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
-.then(response => {
-    let items
-    items = response.data
+    .then(response => {
+        let items
+        items = response.data
 
-    items.forEach(element => {
-        let itemsEl = element
+        items.forEach(element => {
+            let itemsEl = element
 
-        let {albumId, id, title, url, thumbnailUrl} = itemsEl
+            let { albumId, id, title, url, thumbnailUrl } = itemsEl
 
-        console.log(itemsEl)
+            console.log(itemsEl)
 
-        console.log(albumId);
-        
+            console.log(albumId);
 
-        
+            let markup = `
+                <div class="card">
+                <div class="circle"></div>
+                <div class="img">
+                    <img src=${url} alt="">
+                    <p>${title}</p>
+                </div>
+                
+            </div>
+        `
+        mainEl.insertAdjacentHTML("beforeend", markup)
+
+        })
+
+
     })
-    
-    
-})
 
 
 
