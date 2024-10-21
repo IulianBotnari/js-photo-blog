a50e248ae9a9a8c857930d42754b6fb89645a1e6
IulianBotnari <iulianbotnari93@gmail.com>
2024-10-21 15:23:06 +0200

    Update

--- asset/js/main.js
+++ asset/js/main.js
@@ -20,7 +20,7 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
                 <div class="card_mobile card_tablet">
                 <div class="circle"></div>
                 <div class="img">
-                    <img src=${url} alt="">
+                    <img class="a${id}" src=${url} alt="">
                     <p>${title}</p>
                 </div>
                 
@@ -28,27 +28,87 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
         `
             mainEl.insertAdjacentHTML("beforeend", markup)
 
+            
+        })
+        let imagesa1 = document.querySelector(".a1")
+        let imagesa2 = document.querySelector(".a2")
+        let imagesa3 = document.querySelector(".a3")
+        let imagesa4 = document.querySelector(".a4")
+        let imagesa5 = document.querySelector(".a5")
+        let imagesa6 = document.querySelector(".a6")
+
+        let images1 = document.querySelector(".a1").src
+        let images2 = document.querySelector(".a2").src
+        let images3 = document.querySelector(".a3").src
+        let images4 = document.querySelector(".a4").src
+        let images5 = document.querySelector(".a5").src
+        let images6 = document.querySelector(".a6").src
+
+
+
+        let cards = document.querySelector(".pre_overlay")
+
+
+        imagesa1.addEventListener("click", () => {
+            cards.classList.add("d_none")
+            overlay.insertAdjacentHTML("beforeend", `
+                <button class="btn">click</button>
+                <img src="${images1}">`)
+            let btn = document.querySelector(".btn")
+
+            btn.addEventListener("click", () =>{
+                cards.classList.remove("d_none")
+                overlay.classList.add("d_none")
+
+                })
+    
+        })
+
+        imagesa2.addEventListener("click", () => {
+            cards.classList.add("d_none")
+            overlay.insertAdjacentHTML("beforeend", `
+                <button class="btn">click</button>
+                <img src="${images2}">`)
+    
+        })
+
+        imagesa3.addEventListener("click", () => {
+            cards.classList.add("d_none")
+            overlay.insertAdjacentHTML("beforeend", `
+                <button class="btn">click</button>
+                <img src="${images3}">`)
+    
+        })
+
+        imagesa4.addEventListener("click", () => {
+            cards.classList.add("d_none")
+            overlay.insertAdjacentHTML("beforeend", `
+                <button class="btn">click</button>
+                <img src="${images4}">`)
+    
+        })
+
+        imagesa5.addEventListener("click", () => {
+            cards.classList.add("d_none")
+            overlay.insertAdjacentHTML("beforeend", `
+                <button class="btn">click</button>
+                <img src="${images5}">`)
+    
+        })
+
+        imagesa6.addEventListener("click", () => {
+            cards.classList.add("d_none")
+            overlay.insertAdjacentHTML("beforeend", `
+                <button class="btn">click</button>
+                <img src="${images6}">`)
+    
         })
 
 
-        let images = document.querySelectorAll(".img")
-        let imageSrc = document.querySelector("img").src
-        
-        
-        
-        
-        
-        
-        images.forEach((image) => {
-            image.addEventListener("click", () => {
-        
-                overlay.insertAdjacentHTML("beforeend", `<img src=${imageSrc}>`)
-        
-            })
-        })
-
-    })    
-
+    })
+    
+    
+    
     
 
 
